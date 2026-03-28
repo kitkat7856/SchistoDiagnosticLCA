6149566eba0f9bd9d7a413be587960be03ef6cf6
Katharine Hopkinson <kat.hopk01@gmail.com>
2026-03-28 20:59:53 +0000

    Using ROCR creation function to generate ROC curves, to make code neater

--- Figures.R
+++ Figures.R
@@ -1,4 +1,3 @@
-
 ### FIGURES ####
 
 #### RAW DATA ####
@@ -166,6 +165,7 @@ ggplot(dbs_logcurve, aes(x=x*24,y=meanline)) +
   theme_bw()
 
 ### Status at each timepoint #####
+# Posterior probability estimates #
 timepoints<- c("PreT", "3wk", "9wk", "6mo")
 prob<- select(out, starts_with("prob["))
 prob.samples<- prob[sample(40000, 500),]
@@ -202,9 +202,9 @@ ggplot(z, aes(x=factor(tmp, level=timepoints), y=value))+
   ylab("Proportion of population shedding")+
   theme_bw()
 
-#######
+####### Posterior status ##
 status<- select(out, starts_with("Status"))
-status.samples<- status[sample(40000,1000),]
+status.samples<- status[sample(40000,500),]
 status1<- status.samples[,1:210]
 status2<- status.samples[,211:420]
 status3<- status.samples[,421:630]
@@ -257,91 +257,32 @@ ggplot(prev.df, aes(x=factor(tmp, level=timepoints), y=total)) +
   ylab("Estimated prevalence") +
   theme_bw()
 
-#######
-
 #### ROC Curves ####
-#### ROC CT ####
+source("Functions.R")
+
 #status 1 = uninfected(1), status 2 and 3 = infected (2)
-status.samples[status.samples==3]<-2
+status_samples_bin[status.samples==3]<-2
 
-#split status by timepoint, each column is a posterior sample, pair with corresponding CT values
-status.1<- data.frame(CT=-ct[,1], t(status.samples[,1:210])) #timepoint 1
-status.2<- data.frame(CT=-ct[,2], t(status.samples[,211:420]))#timepoint 2
-status.3<- data.frame(CT=-ct[,3], t(status.samples[,421:630])) #timepoint 3
-status.4<- data.frame(CT=-ct[,4], t(status.samples[,631:840]))#timepoint 4
+#### ROC Stool qPCR CT ####
+roc_ct_1<- createROC(-ct,1,status_samples_bin)
+roc_ct_2<- createROC(-ct,2,status_samples_bin)
+roc_ct_3<- createROC(-ct,3,status_samples_bin)
+roc_ct_4<- createROC(-ct,4,status_samples_bin)
 
+#### CT all time points, get cutoff
+predictions_all<- rep(list(na.omit(c(-ct))),500)
 
-#### pre T ROC 
-#removeNA
-status.1<- status.1[!is.na(status.1$CT),]
-#predictions for ROCR- predictions= CT data, list of 500 for each sample
-preTpred<- rep(list(status.1$CT),500)
-#labels for ROCR- labels = status
-preTlabels<- as.list(status.1[,2:501])
+labels_all<-status_samples_bin[,which(!is.na(c(ct)))] 
+labels_all_list<-as.list(as.data.frame(t(labels_all))) 
 
-pred.1<- prediction(preTpred, preTlabels)
-roc.ct.1<-performance(pred.1, "tpr", "fpr")
-ss.ct.1<-performance(pred.1, measure="sens", x.measure = "spec")
+pred_all<- prediction(predictions_all, labels_all_list) 
+ss_ct<- performance(pred_all,"sens","spec") 
+getoptcut(ss_ct,500)
 
-#### 3 week ROC
-#removeNA
-status.2<- status.2[!is.na(status.2$CT),]
-#predictions for ROCR- predictions= CT data, list of 500 for each sample
-wk3pred<- rep(list(status.2$CT),500)
-#labels for ROCR- labels = status
-wk3labels<- as.list(status.2[,2:501])
-
-pred.2<- prediction(wk3pred, wk3labels)
-roc.ct.2<-performance(pred.2, "tpr","fpr")
-ss.ct.2<-performance(pred.2, "sens", "spec")
-plot(ss.ct.2)
-plot(roc.ct.2)
-
-#### 9 week ROC
-#removeNA
-status.3<- status.3[!is.na(status.3$CT),]
-#predictions for ROCR- predictions= CT data, list of 500 for each sample
-wk9pred<- rep(list(status.3$CT),500)
-#labels for ROCR- labels = status
-wk9labels<- as.list(status.3[,2:501])
-
-pred.3<- prediction(wk9pred, wk9labels)
-roc.ct.3<-performance(pred.3, "tpr",  "fpr")
-ss.ct.3<-performance(pred.3, "sens", "spec")
-plot(ss.ct.3)
-plot(roc.ct.3)
-
-### 6 month ROC ####
-status.4<- status.4[!is.na(status.4$CT),]
-#predictions for ROCR- predictions= CT data, list of 500 for each sample
-mnth6pred<- rep(list(status.4$CT),500)
-#labels for ROCR- labels = status
-mnth6labels<- as.list(status.4[,2:501])
-
-#roc
-pred.4<- prediction(mnth6pred, mnth6labels)
-roc.ct.4<-performance(pred.4, "tpr", "fpr")
-ss.ct.4<-performance(pred.4, "sens", "spec")
-plot(ss.ct.4)
-plot(roc.ct.4)
-
-### plot and save roc final figures ####
-cut1<- getoptcut(ss.ct.1,500)
-cut2<- getoptcut(ss.ct.2,500)
-cut3<- getoptcut(ss.ct.3,500)
-cut4<- getoptcut(ss.ct.4,500)
-
-ctstatus<- data.frame(ct=c(-ct), t(status.samples))
-ctstatus<- ctstatus[!is.na(ctstatus$ct),]
-pred<- rep(list(ctstatus$ct),500)
-label<- as.list(ctstatus[,2:501])
-pred.ct<- prediction(pred,label)
-perf<- performance(pred.ct,"sens", "spec")
-
-
-pdf("ROC CT curves.pdf")
+### plot and save roc final figures ###
+pdf("ROC Stool CT curves.pdf")
 par(mfrow=c(2,2))
-plot(roc.ct.1,
+plot(roc_ct_1,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -353,7 +294,7 @@ plot(roc.ct.1,
      col= "blue",
      main = "Pre-Treatment, Stool qPCR")
 abline(0,1)
-plot(roc.ct.2,
+plot(roc_ct_2,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -365,7 +306,7 @@ plot(roc.ct.2,
      col= "blue",
      main = "Three weeks, Stool qPCR")
 abline(0,1)
-plot(roc.ct.3,
+plot(roc_ct_3,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -377,7 +318,7 @@ plot(roc.ct.3,
      col= "blue",
      main = "nine weeks, Stool qPCR")
 abline(0,1)
-plot(roc.ct.4,
+plot(roc_ct_4,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -394,67 +335,16 @@ dev.off()
 
 
 #### ROC KK ####
-#data frames separating gscore values by each timepoint, status for each tmp
-status.1<- data.frame(kk=kkmean[,1], t(status.samples[,1:210])) #timepoint 1
-status.2<- data.frame(kk=kkmean[,2], t(status.samples[,211:420]))#timepoint 2
-status.3<- data.frame(kk=kkmean[,3], t(status.samples[,421:630])) #timepoint 3
-status.4<- data.frame(kk=kkmean[,4], t(status.samples[,631:840]))#timepoint 4
-
-#removeNA
-status.1<- status.1[!is.na(status.1$kk),]
-#predictions for ROCR- predictions= gscore data, list of 500 for each sample
-preTpred<- rep(list(status.1$kk),500)
-#labels for ROCR- labels = statu s
-preTlabels<- as.list(status.1[,2:501])
-
-pred.1<- prediction((preTpred),(preTlabels))
-roc.kk.1<-performance(pred.1, measure="tpr", x.measure = "fpr")
-ss.kk.1<-performance(pred.1, measure="sens", x.measure = "spec")
-
-### 3 week ROC
-status.2<- status.2[!is.na(status.2$kk),]
-#predictions for ROCR- predictions= kkscore data, list of 500 for each sample
-wk3pred<- rep(list(status.2$kk),500)
-#labels for ROCR- labels = status
-wk3labels<- as.list(status.2[,2:501])
-
-pred.2<- prediction(wk3pred, wk3labels)
-roc.kk.2<-performance(pred.2, measure="tpr", x.measure = "fpr")
-ss.kk.2<-performance(pred.2, measure="sens", x.measure = "spec")
-plot(ss.kk.2)
-plot(roc.kk.2)
-
-### 9 week ROC
-status.3<- status.3[!is.na(status.3$kk),]
-#predictions for ROCR- predictions= kkscore data, list of 500 for each sample
-wk9pred<- rep(list(status.3$kk),500)
-#labels for ROCR- labels = status
-wk9labels<- as.list(status.3[,2:501])
-
-pred.3<- prediction(wk9pred, wk9labels)
-roc.kk.3<-performance(pred.3, measure="tpr", x.measure = "fpr")
-ss.kk.3<-performance(pred.3, measure="sens", x.measure = "spec")
-plot(ss.kk.3)
-plot(roc.kk.3)
-
-### 6 month ROC
-status.4<- status.4[!is.na(status.4$kk),]
-#predictions for ROCR- predictions= kkscore data, list of 500 for each sample
-mnth6pred<- rep(list(status.4$kk),500)
-#labels for ROCR- labels = status
-mnth6labels<- as.list(status.4[,2:501])
-
-pred.4<- prediction(mnth6pred, mnth6labels)
-roc.kk.4<-performance(pred.4, measure="tpr", x.measure = "fpr")
-ss.kk.4<-performance(pred.4, measure="sens", x.measure = "spec")
-plot(ss.kk.4)
-plot(roc.kk.4)
+roc_kk_1<- createROC(kk,1,status_samples_bin)
+roc_kk_2<- createROC(kk,2,status_samples_bin)
+roc_kk_3<- createROC(kk,3,status_samples_bin)
+roc_kk_4<- createROC(kk,4,status_samples_bin)
 
 ### plot and save roc final figures ####
 
 pdf("ROC KK curves.pdf")
 par(mfrow=c(2,2))
-plot(roc.kk.1,
+plot(roc_kk_1,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -463,7 +353,7 @@ plot(roc.kk.1,
      col= "brown",
      main = "Pre-Treatment, KK")
 abline(0,1)
-plot(roc.kk.2,
+plot(roc_kk_2,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -473,7 +363,7 @@ plot(roc.kk.2,
      col= "brown",
      main = "three weeks, KK") 
 abline(0,1)
-plot(roc.kk.3,
+plot(roc_kk_3,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -483,7 +373,7 @@ plot(roc.kk.3,
      col= "brown",
      main = "nine weeks, KK")
 abline(0,1)
-plot(roc.kk.4,
+plot(roc_kk_4,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -497,74 +387,16 @@ abline(0,1)
 dev.off()
 
 ##### ROC GSCORE ######
-#data frames separating gscore values by each timepoint, status for each tmp
-status.1<- data.frame(G=gscore[,1]+1, t(status.samples[,1:210])) #timepoint 1
-status.2<- data.frame(G=gscore[,2]+1, t(status.samples[,211:420]))#timepoint 2
-status.3<- data.frame(G=gscore[,3]+1, t(status.samples[,421:630])) #timepoint 3
-status.4<- data.frame(G=gscore[,4]+1, t(status.samples[,631:840]))#timepoint 4
+roc_G_1<- createROC(gscore+1,1,status_samples_bin)
+roc_G_2<- createROC(gscore+1,2,status_samples_bin)
+roc_G_3<- createROC(gscore+1,3,status_samples_bin)
+roc_G_4<- createROC(gscore+1,4,status_samples_bin)
 
-#removeNA
-status.1<- status.1[!is.na(status.1$G),]
-#predictions for ROCR- predictions= gscore data, list of 500 for each sample
-preTpred<- rep(list(status.1$G),500)
-#labels for ROCR- labels = status
-preTlabels<- as.list(status.1[,2:501])
-
-pred.1<- prediction((preTpred),(preTlabels))
-roc.G.1<-performance(pred.1, measure="tpr", x.measure = "fpr")
-ss.G.1<-performance(pred.1, measure="sens", x.measure = "spec")
-plot(ss.G.1)
-plot(roc.G.1)
-
-
-### 3 week ROC
-status.2<- status.2[!is.na(status.2$G),]
-#predictions for ROCR- predictions= gscore data, list of 500 for each sample
-wk3pred<- rep(list(status.2$G),500)
-#labels for ROCR- labels = status
-wk3labels<- as.list(status.2[,2:501])
-
-pred.2<- prediction(wk3pred, wk3labels)
-roc.G.2<-performance(pred.2, measure="tpr", x.measure = "fpr")
-ss.G.2<-performance(pred.2, measure="sens", x.measure = "spec")
-plot(ss.G.2)
-plot(roc.G.2)
-
-### 9 week ROC
-status.3<- status.3[!is.na(status.3$G),]
-#predictions for ROCR- predictions= gscore data, list of 500 for each sample
-wk9pred<- rep(list(status.3$G),500)
-#labels for ROCR- labels = status
-wk9labels<- as.list(status.3[,2:501])
-
-pred.3<- prediction(wk9pred, wk9labels)
-roc.G.3<-performance(pred.3, measure="tpr", x.measure = "fpr")
-ss.G.3<-performance(pred.3, measure="sens", x.measure = "spec")
-plot(ss.G.3)
-plot(roc.G.3)
-
-### 6 month ROC
-status.4<- status.4[!is.na(status.4$G),]
-#predictions for ROCR- predictions= gscore data, list of 500 for each sample
-mnth6pred<- rep(list(status.4$G),500)
-#labels for ROCR- labels = status
-mnth6labels<- as.list(status.4[,2:501])
-
-pred.4<- prediction(mnth6pred, mnth6labels)
-roc.G.4<-performance(pred.4, measure="tpr", x.measure = "fpr")
-ss.G.4<-performance(pred.4, measure="sens", x.measure = "spec")
-plot(ss.G.4)
-plot(roc.G.4)
-
-### plot and save roc final figures ####
-cut1<- getoptcut(ss.G.1,1)
-cut2<- getoptcut(ss.G.2,500)
-cut3<- getoptcut(ss.G.3,500)
-cut4<- getoptcut(ss.G.4,500)
+### Create PDF of ROC GSCORE Plots ##
 
 pdf("ROC Gscore curves.pdf")
 par(mfrow=c(2,2))
-plot(roc.G.1,
+plot(roc_G_1,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -575,7 +407,7 @@ plot(roc.G.1,
      col= "orange",
      main = "Pre-Treatment,Gscore")
 abline(0,1)
-plot(roc.G.2,
+plot(roc_G_2,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -587,7 +419,7 @@ plot(roc.G.2,
      col= "orange",
      main = "three weeks,Gscore") 
 abline(0,1)
-plot(roc.G.3,
+plot(roc_G_3,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -599,7 +431,7 @@ plot(roc.G.3,
      col= "orange",
      main = "nine weeks, Gscore")
 abline(0,1)
-plot(roc.G.4,
+plot(roc_G_4,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -617,70 +449,16 @@ dev.off()
 
 
 ##### ROC DBS pcr######
-#data frames separating pcr values by each timepoint, status for each tmp
-status.1<- data.frame(pcr=-dbs.ct[,1], t(status.samples[,1:210])) #timepoint 1
-status.2<- data.frame(pcr=-dbs.ct[,2], t(status.samples[,211:420]))#timepoint 2
-status.3<- data.frame(pcr=-dbs.ct[,3], t(status.samples[,421:630])) #timepoint 3
-status.4<- data.frame(pcr=-dbs.ct[,4], t(status.samples[,631:840]))#timepoint 4
-
-#removeNA
-status.1<- status.1[!is.na(status.1$pcr),]
-#predictions for ROCR- predictions= pcr data, list of 500 for each sample
-preTpred<- rep(list(status.1$pcr),500)
-#labels for ROCR- labels = status
-preTlabels<- as.list(status.1[,2:501])
-
-pred.1<- prediction((preTpred),(preTlabels))
-roc.pcr.1<-performance(pred.1, measure="tpr", x.measure = "fpr")
-ss.pcr.1<-performance(pred.1, measure="sens", x.measure = "spec")
-plot(ss.pcr.1)
-plot(roc.pcr.1)
+roc_dbs_1<- createROC(dbs,1,status_samples_bin)
+roc_dbs_2<- createROC(dbs,2,status_samples_bin)
+roc_dbs_3<- createROC(dbs,3,status_samples_bin)
+roc_dbs_4<- createROC(dbs,4,status_samples_bin)
 
 
-### 3 week ROC
-status.2<- status.2[!is.na(status.2$pcr),]
-#predictions for ROCR- predictions= pcr data, list of 500 for each sample
-wk3pred<- rep(list(status.2$pcr),500)
-#labels for ROCR- labels = status
-wk3labels<- as.list(status.2[,2:501])
-
-pred.2<- prediction(wk3pred, wk3labels)
-roc.pcr.2<-performance(pred.2, measure="tpr", x.measure = "fpr")
-ss.pcr.2<-performance(pred.2, measure="sens", x.measure = "spec")
-plot(ss.pcr.2)
-plot(roc.pcr.2)
-
-### 9 week ROC
-status.3<- status.3[!is.na(status.3$pcr),]
-#predictions for ROCR- predictions= pcr data, list of 500 for each sample
-wk9pred<- rep(list(status.3$pcr),500)
-#labels for ROCR- labels = status
-wk9labels<- as.list(status.3[,2:501])
-
-pred.3<- prediction(wk9pred, wk9labels)
-roc.pcr.3<-performance(pred.3, measure="tpr", x.measure = "fpr")
-ss.pcr.3<-performance(pred.3, measure="sens", x.measure = "spec")
-plot(ss.pcr.3)
-plot(roc.pcr.3)
-
-### 6 month ROC
-status.4<- status.4[!is.na(status.4$pcr),]
-#predictions for ROCR- predictions= pcr data, list of 500 for each sample
-mnth6pred<- rep(list(status.4$pcr),500)
-#labels for ROCR- labels = status
-mnth6labels<- as.list(status.4[,2:501])
-
-pred.4<- prediction(mnth6pred, mnth6labels)
-roc.pcr.4<-performance(pred.4, measure="tpr", x.measure = "fpr")
-ss.pcr.4<-performance(pred.4, measure="sens", x.measure = "spec")
-plot(ss.pcr.4)
-plot(roc.pcr.4)
-
-### plot and save roc final figures ####
-
+### plot and save ROC final figures ####
 pdf("ROC DBS pcr curves.pdf")
 par(mfrow=c(2,2))
-plot(roc.pcr.1,
+plot(roc_dbs_1,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -691,7 +469,7 @@ plot(roc.pcr.1,
      col= "red",
      main = "Pre-Treatment, DBS qPCR")
 abline(0,1)
-plot(roc.pcr.2,
+plot(roc_dbs_2,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -703,7 +481,7 @@ plot(roc.pcr.2,
      col= "red",
      main = "three weeks, DBS qpcr") 
 abline(0,1)
-plot(roc.pcr.3,
+plot(roc_dbs_3,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -715,7 +493,7 @@ plot(roc.pcr.3,
      col= "red",
      main = "nine weeks, DBS qpcr")
 abline(0,1)
-plot(roc.pcr.4,
+plot(roc_dbs_4,
      avg= "threshold",
      spread.estimate= "stddev",
      spread.scale=2,
@@ -730,10 +508,11 @@ abline(0,1)
 
 dev.off()
 
+### Sensitivity Specificity
 
-sesp<-matrix(NA,4,8)
-sesp[1,]<- as.numeric(c(getss(ss.kk.1,500,0.1)[,1:2],getss(ss.kk.2,500,0.1)[,1:2],getss(ss.kk.3,500,0.15)[,1:2],getss(ss.kk.4,500,0.1)[,1:2]))
-sesp[2,]<- as.numeric(c(getss(ss.ct.1,500,-40)[,1:2],getss(ss.ct.2,500,-40)[,1:2],getss(ss.ct.3,500,-40)[,1:2],getss(ss.ct.4,500,-40)[,1:2]))
-sesp[3,]<- as.numeric(c(getss(ss.G.1,500,3)[,1:2],getss(ss.G.2,500,3)[,1:2],getss(ss.G.3,500,3)[,1:2],getss(ss.G.4,500,3)[,1:2]))
-sesp[4,]<- as.numeric(c(getss(ss.pcr.1,500,-39)[,1:2],getss(ss.pcr.2,500,-39)[,1:2],getss(ss.pcr.3,500,-39)[,1:2],getss(ss.pcr.4,500,-39)[,1:2]))
+#sesp<-matrix(NA,4,8)
+#sesp[1,]<- as.numeric(c(getss(ss.kk.1,500,0.1)[,1:2],getss(ss.kk.2,500,0.1)[,1:2],getss(ss.kk.3,500,0.15)[,1:2],getss(ss.kk.4,500,0.1)[,1:2]))
+#sesp[2,]<- as.numeric(c(getss(ss.ct.1,500,-38)[,1:2],getss(ss.ct.2,500,-38)[,1:2],getss(ss.ct.3,500,-38)[,1:2],getss(ss.ct.4,500,-38)[,1:2]))
+#sesp[3,]<- as.numeric(c(getss(ss.G.1,500,3)[,1:2],getss(ss.G.2,500,3)[,1:2],getss(ss.G.3,500,3)[,1:2],getss(ss.G.4,500,3)[,1:2]))
+#sesp[4,]<- as.numeric(c(getss(ss.pcr.1,500,-38)[,1:2],getss(ss.pcr.2,500,-38)[,1:2],getss(ss.pcr.3,500,-38)[,1:2],getss(ss.pcr.4,500,-38)[,1:2]))
 
